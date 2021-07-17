8c56072daac1e3afd466d3ff3d5314d432795156
MarcioMAlmeida <marcioa.marcioa@gmail.com>
2021-07-17 10:05:22 -0300

    configurando orm e criando migrations

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
-
 import { router } from "./routes";
+import "./database";
 
 const app = express();
-
 app.use(express.json());
-
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
