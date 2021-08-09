d21b5a5e68fcbb86a2bd6f0e991ba304d8a315f5
MarcioMAlmeida <marcioa.marcioa@gmail.com>
2021-08-09 13:44:37 -0300

    criei o createproductservice

--- src/services/CreateProductService.ts
+++ src/services/CreateProductService.ts
@@ -11,11 +11,30 @@ interface IProductRequest {
 class CreateProductService {
 
     async execute({ name, description, cost, quantity }: IProductRequest) {
-        const productsRepositories = getCustomRepository(ProductsRepositories);
+        const productsRepository = getCustomRepository(ProductsRepositories);
 
-        if (!name || !description || !cost) {
-            throw new Error("Invalid product!");
+        if (!name) {
+            throw new Error("Invalid name!");
         }
+
+        const productAlreadyExists = await productsRepository.findOne({
+            name
+        })
+
+        if (productAlreadyExists) {
+            throw new Error("Product already exists!");
+        }
+
+        const product = productsRepository.create({
+            name,
+            description,
+            cost,
+            quantity
+        });
+
+        await productsRepository.save(product);
+
+        return product;
 
     }
 }
