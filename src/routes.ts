d8e8e854d0c7f7409b6576685f8fd2ef9ff629d1
MarcioMAlmeida <marcioa.marcioa@gmail.com>
2021-08-23 13:11:41 -0300

    esqueci de fazer commits

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 
 import { CreateProductController } from "./controllers/CreateProductController";
+import { CreateUserController } from "./controllers/CreateUserController";
+import { ListUserController } from "./controllers/ListUserController";
 
 const router = Router();
 
 const createProductController = new CreateProductController();
+const createUserController = new CreateUserController();
+const listUserController = new ListUserController();
 
 router.get("/", (req, res, next) => {
     res.status(200).send({
@@ -13,5 +17,8 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/products", createProductController.handle);
+
+router.post("/users", createUserController.handle);
+router.get("/users", listUserController.handle);
 
 export { router };
